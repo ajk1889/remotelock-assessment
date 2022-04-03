6563f42745dee54bce70463f04a118abfb638b25
Amaljith K <amaljith.k@bigbinary.com>
2022-04-03 12:27:34 +0530

    Updated tests

--- spec/functional/models/person_spec.rb
+++ spec/functional/models/person_spec.rb
@@ -12,20 +12,6 @@ RSpec.describe 'Person model test' do
     end
     let(:person) { Person.new(**attributes) }
 
-    it 'should have names properly assigned' do
-      %i[first_name last_name].each do |key|
-        expect(person.send(key)).to eq(attributes[key])
-      end
-    end
-
-    it 'should have birthdate properly assigned' do
-      expect(person.birthdate.strftime("%-m/%-d/%Y")).to eq("4/5/1947")
-    end
-
-    it 'should have city properly assigned' do
-      expect(person.city.long_name).to eq("Chicago")
-    end
-
     it 'should convert to string in `<first_name>, <city>, <birth_date M/D/YYYY>` format' do
       expect(person.to_s).to eq("Amaljith, Chicago, 4/5/1947")
     end
